29ffa8774167b111da80ce6ea0700ea33db85c0a
Basit Shaikh <shaikhbasit279@gmail.com>
2024-12-26 02:12:40 +0500

    add some features

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 const displayResult = document.querySelector('#display-result');
 const displaySelection = document.querySelector('#selections-display');
+const displayScore = document.querySelector('#display-score');
+const resetBtn = document.querySelector('#reset-button');
 
-let result;
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+}
+
+    displayScore.innerHTML = `Wins: ${score.wins}, 
+losses:${score.losses}, Ties:${score.ties}`;
 
 document.querySelector('#rock-btn')
   .addEventListener('click', () => {
@@ -18,12 +27,26 @@ document.querySelector('#scissors-btn')
     playGame('scissors');
 })
 
+resetBtn.addEventListener('click', () => {
+
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+  }
+
+  localStorage.removeItem('score');
+  displayScore.innerHTML = `Wins: ${score.wins}, 
+losses:${score.losses}, Ties:${score.ties}`;
+
+})
+
 
 function playGame(playerMove) {
 
   const computerPick = computerMove();
 
-   result = '';
+   let result = '';
 
   if(playerMove === 'rock') {
 
@@ -62,6 +85,8 @@ function playGame(playerMove) {
     You <img class="images-dev" src="Images/${playerMove}-emoji.png">
 <img class="images-dev" src="Images/${computerPick}-emoji.png"> computer`;
 
+  updateScore(result);
+
 }
 
 function computerMove() {
@@ -79,5 +104,22 @@ function computerMove() {
     }
 
     return computerPick;
+
+}
+
+function updateScore(result) {
+
+  if(result === 'You Win!') {
+    score.wins++;
+  } else if(result === 'You Lose!') {
+    score.losses++;
+  } else if(result === 'Tie.') {
+    score.ties++;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+
+  displayScore.innerHTML = `Wins: ${score.wins}, 
+losses:${score.losses}, Ties:${score.ties}`;
 
 }
